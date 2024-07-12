f1552ac99b2a29018524288217a7a67cb761f696
Oluwatobiloba Abidoye <86835123+tobiabidoye@users.noreply.github.com>
2024-07-12 00:08:20 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import socket
+# import socket
 
 
 def main():
@@ -6,9 +6,9 @@ def main():
     print("Logs from your program will appear here!")
 
 
-
-     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-     server_socket.accept() # wait for client
+    #
+    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
