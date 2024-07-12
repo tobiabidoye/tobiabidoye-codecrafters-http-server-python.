2055b0e31a6764508184a4fbf4e341c628dfc27e
Oluwatobiloba Abidoye <86835123+tobiabidoye@users.noreply.github.com>
2024-07-12 00:12:31 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,12 +2,8 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-
-
-
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
