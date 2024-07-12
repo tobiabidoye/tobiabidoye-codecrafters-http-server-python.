a9537d329ad005d85e10182186fdad148098c479
Oluwatobiloba Abidoye <86835123+tobiabidoye@users.noreply.github.com>
2024-07-12 16:02:21 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,23 +2,23 @@ import socket
 
 MAXBYTES = 1024
 def handle_connection(conn):
-    conn.recv(MAXBYTES) #specifying max amount of bytes
+    conn.recv(MAXBYTES)  # specifying max amount of bytes
     response = "HTTP/1.1 200 OK\r\n\r\n"
-    conn.send(response.encode()) #.encode converts string into bytes
-
+    conn.send(response.encode())  # .encode converts string into bytes
+    conn.close()
 
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-    # .accept() is a method that waits for clients
+
+    print("Server is listening for connections...")
     while True:
         # while loop which waits for a connection
         print("Waiting for a connection...")
-        conn, addr = server_socket.accept() #this gives us the client details
-        print(f"connection from: {addr} has been established")
+        conn, addr = server_socket.accept()  # this gives us the client details
+        print(f"connection from:{addr} has been established")
         handle_connection(conn)
-        conn.close()
+
 
 
 
