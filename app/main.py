6b5e8ea026c43ec5a5a8266ef70d35080a24b8dd
Tobi Abidoye <tobiabidoye14@gmail.com>
2024-07-20 09:35:36 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def handle_connection(conn):
         toSend = path[6:]
         contentType = "text/plain\r\n"
         contentLen = len(toSend)
-        response = response + contentType + str(toSend) + contentLen + "\r\n"
+        response = response + contentType + toSend + "Content-Length:" + str(contentLen) + "\r\n"
     else:
         if path is not "/":
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
