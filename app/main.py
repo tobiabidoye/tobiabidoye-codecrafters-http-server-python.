2855331c2b7cf2e27cc9bb73b3567c3afaa0cf7a
Tobi Abidoye <tobiabidoye14@gmail.com>
2024-08-10 15:37:22 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 import socket
 import threading
+import argparse
+import os
 
 MAXBYTES = 1024
+
+
 def handle_connection(conn):
     data = conn.recv(MAXBYTES)  # specifying max amount of bytes
 
@@ -11,6 +15,7 @@ def handle_connection(conn):
     response = "HTTP/1.1 200 OK\r\n\r\n"
 
     if "echo" in path:
+
         response = "HTTP/1.1 200 OK\r\n"
         toSend = path[6:]
         contentType = "Content-Type: text/plain\r\n"
@@ -18,14 +23,37 @@ def handle_connection(conn):
         response = response + contentType + "Content-Length: " + str(contentLen) + "\r\n\r\n" + toSend
 
     elif "user-agent" in path:
+
         response = "HTTP/1.1 200 OK\r\n"
         agent, agentname = headers_list[1].split()
         contentType = "Content-Type: text/plain\r\n"
         contentLen = len(agentname)
         response = response + contentType + "Content-Length: " + str(contentLen) + "\r\n\r\n" + agentname
 
+    elif path.startswith("/files"):
+
+        filename = path[7:] #this is where filename is contained
+        filepath = os.path.join(dirpath, filename)
+        if os.path.exists(filepath) and os.path.isfile(filepath):
+            
+            with open(filepath, 'rb') as file: 
+                #open file in binary mode and read it
+                content = file.read()
+            
+            response = "HTTP/1.1 200 OK\r\n"
+            response += "Content-Type: application/octet-stream\r\n"
+            response += f"Content-Length: {len(content)}\r\n\r\n"
+            conn.send(response.encode() + content)
+            
+        else: 
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            conn.send(response.encode())
+        return
+
     else:
+
         if path != "/":
+
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
     conn.send(response.encode())  # .encode converts string into bytes
@@ -34,7 +62,12 @@ def handle_connection(conn):
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
+    parser = argparse.ArgumentParser() #parser initialization
+    parser.add_argument('--directory',required= True, help= 'parsing command line argument for directory flag') #parsing for directory flag
+    args = parser.parse_args()
+    global dirpath
+    dirpath = args.directory
+    
     print("Server is listening for connections...")
     while True:
         # while loop which waits for a connection
